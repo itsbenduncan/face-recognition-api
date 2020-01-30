bcaa1deb900c9058191a01025568925a850d8bfd
Ben Duncan <benduncan@Bens-MacBook-Pro.local>
2020-01-29 20:45:03 -1000

    fixing image request

--- controllers/image.js
+++ controllers/image.js
@@ -15,7 +15,7 @@ const handleApiCall = (req, res) => {
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
-    return db('users').where('id', '=', id)
+    db('users').where('id', '=', id)
         .increment('entries', 1)
         .returning('entries') 
         .then(entries => {
